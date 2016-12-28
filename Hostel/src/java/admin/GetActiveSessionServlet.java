3e39455143460089f762c3102daf0033992da68a
Pang Cheng Hao <pangchenghao94@gmail.com>
2016-12-28 18:06:44 +0800

    o0o

--- Hostel/src/java/admin/GetActiveSessionServlet.java
+++ Hostel/src/java/admin/GetActiveSessionServlet.java
@@ -5,13 +5,19 @@
  */
 package admin;
 
+import bean.Session;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import jdbc.JDBCUtility;
 
 /**
  *
@@ -20,6 +26,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "GetActiveSessionServlet", urlPatterns = {"/GetActiveSessionServlet"})
 public class GetActiveSessionServlet extends HttpServlet {
 
+    private JDBCUtility jdbcUtility;
+    private Connection con;
+    
+    public void init() throws ServletException
+    {
+        String driver = "com.mysql.jdbc.Driver";
+
+        String dbName = "db_hostel";
+        String url = "jdbc:mysql://localhost/" + dbName + "?";
+        String userName = "root";
+        String password = "";
+
+        jdbcUtility = new JDBCUtility(driver,
+                                      url,
+                                      userName,
+                                      password);
+
+        jdbcUtility.jdbcConnect();
+        con = jdbcUtility.jdbcGetConnection();
+        jdbcUtility.prepareSQLStatement();
+    }           
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -31,19 +59,47 @@ public class GetActiveSessionServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GetActiveSessionServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GetActiveSessionServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        
+        ArrayList sessions = new ArrayList();
+        Session session = null;
+        
+        //select all from roomtype
+        try{
+            //select all from session
+            ResultSet rs = jdbcUtility.getPsSelectAllFromSession().executeQuery();
+            
+            while (rs.next()) {     
+                session = new Session();
+                session.setId(rs.getInt("Session_PK"));
+                session.setName(rs.getString("Name"));
+                session.setStatus(rs.getInt("Status"));
+            }
+                
+                if(rs.getInt("Status") == 1)
+                    sessions.add(session);
         }
+        catch (SQLException ex)
+	{
+            while (ex != null)
+            {
+                System.out.println ("SQLState: " +
+                                 ex.getSQLState ());
+                System.out.println ("Message:  " +
+                                 ex.getMessage ());
+		System.out.println ("Vendor:   " +
+                                 ex.getErrorCode ());
+                ex = ex.getNextException ();
+		      System.out.println ("");
+            }
+            
+            System.out.println("Connection to the database error");
+	}
+	catch (java.lang.Exception ex)
+	{
+            ex.printStackTrace ();
+	}       
+        
+        request.setAttribute("sessions",sessions);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
