c8fada2e45385e4718733a186e049e321df084a3
tslim <tzesoon@hotmail.com>
2016-12-28 18:03:49 +0800

    aa

--- Hostel/src/java/auth.java
+++ Hostel/src/java/auth.java
@@ -115,23 +115,27 @@ public class auth implements Filter {
         //get session
         HttpSession session = ((HttpServletRequest) request).getSession();
 
-        //get url of request page
-        String url = ((HttpServletRequest) request).getRequestURL().toString();
-        if (!url.endsWith("Login")) {
-            try {
+        
+        if(session.getAttribute("user") == null)
+        {
+            //get url of request page
+            String url = ((HttpServletRequest) request).getRequestURL().toString();
+            if (url.endsWith("Login")) {
+                try {
 
-                chain.doFilter(request, response);
-            } catch (Throwable t) {
-                // If an exception is thrown somewhere down the filter chain,
-                // we still want to execute our after processing, and then
-                // rethrow the problem after that.
-                problem = t;
-                t.printStackTrace();
+                    chain.doFilter(request, response);
+                } catch (Throwable t) {
+                    // If an exception is thrown somewhere down the filter chain,
+                    // we still want to execute our after processing, and then
+                    // rethrow the problem after that.
+                    problem = t;
+                    t.printStackTrace();
+                }
+            } else {
+                //redirect to Login servlet
+                ((HttpServletResponse) response).sendRedirect("Login");
+
             }
-        } else {
-            //redirect to Login servlet
-            ((HttpServletResponse) response).sendRedirect("Login");
-
         }
 
         doAfterProcessing(request, response);
